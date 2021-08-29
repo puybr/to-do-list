8bc3bc8ce2391c4e56e0e8d06709f96d0700eff2
Chloe Urisohn <chloeurisohn@gmail.com>
2021-08-29 16:56:13 +0100

    add delete function

--- src/controller.js
+++ src/controller.js
@@ -1,12 +1,11 @@
 import taskManager from './to-dos';
+import { format } from 'date-fns';
 
 const todoList = taskManager();
 todoList.renderList();
 const controller = () => {
     const titleField = document.querySelector('#title');
-    const descriptionField = document.querySelector('#description');
     const dateField = document.querySelector('#date');
-    const priorityField = document.querySelector('#priority');
     const taskButton = document.querySelector('#submit');
     const projectButton = document.querySelector('#addproject');
     const projectInput = document.querySelector('#project');
@@ -17,13 +16,13 @@ const controller = () => {
         todos: [{
             title: "code",
             description: "try creating a web interface and a dynamic todo list", 
-            date: "Monday",
+            date: format(new Date(), "yyyy'-'MM'-'dd"),
             priority: "High"
         },
         {
             title: "cook dinner",
             description: "A warming vegan supper with porcini mushrooms, leeks, carrots, and butternut squash.", 
-            date: "Wednesday",
+            date: format(new Date(), "yyyy'-'MM'-'dd"),
             priority: "Medium"
         }
     ]},
@@ -32,13 +31,13 @@ const controller = () => {
         todos: [{
             title: "create a powerpoint presentation",
             description: "for the finacial team",
-            date: "Tuesday",
+            date: format(new Date(), "yyyy'-'MM'-'dd"),
             priority: "Low"
         },
         {
             title: "admin tasks",
             description: "office cleanup", 
-            date: "Friday",
+            date: format(new Date(), "yyyy'-'MM'-'dd"),
             priority: "Low"
         }
     ]
@@ -49,6 +48,7 @@ const controller = () => {
     const renderProjects = () => {
         const container = document.querySelector('#projectnav');
         container.innerHTML = '';
+        document.getElementById("formContainer").style.display = "none";
         // console.log(myProjects[1].todos[0].title);
         myProjects.forEach((project) => {
             const proj = `
@@ -66,6 +66,7 @@ const controller = () => {
     renderProjects();
 
     const addProject = () => {
+        document.getElementById("formContainer").style.display = "none";
         projectButton.addEventListener('click', (e) => {
             e.preventDefault(); // prevent page reloading
             const projectTemplate = {
@@ -94,11 +95,13 @@ const controller = () => {
 
 
     const selectProject = () => {
+        document.getElementById("formContainer").innerHTML = '';
         const list = document.querySelectorAll('.projectlist');
         subTasks.innerHTML = '';
         list.forEach((project) => {
             project.addEventListener('click', (e) => {
                 //this is what happens when you select a project :)
+                document.getElementById("formContainer").style.display = "none";
                 e.preventDefault(); // prevent page reloading
                 document.getElementById("project-todos").innerHTML = '';
                 const p = `<h4 id="project-title">${e.target.innerText}</h4>`;
@@ -130,10 +133,7 @@ const controller = () => {
         addTodo();
         // reset default values
         titleField.value = '';
-        descriptionField.value = '';
         dateField.value = '';
-        priorityField.value = 'low';
-
     });
 
     const renderTodoList = (index) => {
@@ -143,14 +143,14 @@ const controller = () => {
                     <tr class="to-do-row">
                     <td id="delete-me"><input type="radio" id="delete" name="delete"></td>
                     <td>${todo.title}</td>
-                    <td>${todo.description}</td>
                     <td>${todo.date}</td>
-                    <td>${todo.priority}</td>
+                    <td><i class="fas fa-edit"></i></td>
                     </tr>
                 `;
             subTasks.insertAdjacentHTML('afterbegin', t);
         });
         deleteTodo();
+        editTodoList();
     };
 
     const deleteTodo = () => {
@@ -166,18 +166,33 @@ const controller = () => {
         
     };
 
+    const editTodoList = () => {
+        document.getElementById("formContainer").innerHTML = '';
+        const todos = document.querySelectorAll('.to-do-row');
+        todos.forEach((todo) => {
+            todo.addEventListener('click', (e) => {
+                const t = `
+                        <div id="task">
+                        <h1>${e.target.innerText}</h1>
+                        </div>
+                        `;
+                document.getElementById("formContainer").style.display = "block";
+                document.getElementById("formContainer").innerHTML = t;
+            });
+
+        });
+
+    };
+
     const getTodoIndex = (todoName) => {
         const proj = document.querySelector("#project-title").textContent;
         myProjects.forEach((project, projectIndex) => {
             if (project.name === proj) {
                 myProjects[projectIndex].todos.forEach((todo, todoIndex) => {
                     if (todo.title === todoName) {
-                        console.log(todoIndex);
-                        myProjects[projectIndex].todos.splice(todoIndex, 1); // at position index, remove 1 item
+                        myProjects[projectIndex].todos.splice(todoIndex, 1); //at position index, remove 1 item
                         renderTodoList(projectIndex);
-
                     };
-
                 });
             };
         });
@@ -188,9 +203,9 @@ const controller = () => {
             if (p.name === (document.getElementById("project-todos").innerText)) {
                     const todoTemplate = {
                         title: title.value,
-                        description: description.value, 
+                        description: '', 
                         date: date.value,
-                        priority: priority.value
+                        priority: ''
                     };
                     const newTodo = Object.create(todoTemplate);
                     myProjects[index].todos.push(newTodo);
