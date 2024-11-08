e5ab4d4c24a48a52aaaf1f40882ec06ae9d7e9df
puybr <56723522+puybr@users.noreply.github.com>
2024-11-09 00:03:47 +0200

    Update to-dos.js

--- src/to-dos.js
+++ src/to-dos.js
@@ -1,16 +1,15 @@
+"use strict";
+
 const todoManager = (title, description, date, priority) => {
     const deleteTodo = () => {
         document.querySelectorAll('#delete-checkbox').forEach((del) => {
             del.addEventListener('click', (e) => {
                 if (del.checked) {
-                    console.log(myProjects)
-                   
+                    console.log(myProjects);                
                     // getTodoIndex(todo.children[1].textContent);
-                }
+                };
             });
-
-        });
-        
+        });      
     };
 
     return { deleteTodo }
