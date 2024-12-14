2921edddcf2870a6bb97374604acfb21234b3fdd
Chloe Urisohn <56723522+ghost-goblin@users.noreply.github.com>
2024-12-14 14:22:46 +0200

    add tailwindcss

--- src/projects.js
+++ src/projects.js
@@ -7,21 +7,38 @@ const container = document.querySelector('#container');
 const projectManager = () => {
     const renderProject = (project) => {
         const proj = `
-            <div class="projects">
-            <div class="logo"><h1>${project.name}</h1></div>
-            <div><h1 id="addproject" type="submit"><i class="fa fa-trash" aria-hidden="true" id="icon"></i></h1></div>
-            </div>
-            <section class="wrapper">
-                <input type="text" id="title" name="title" placeholder="title" class="logo">
-                <input type="date" id="date" name="date" placeholder="date">
-                <select name="priority" id="priority">
-                    <option value="Low">Low</option>
-                    <option value="Medium">Medium</option>
-                    <option value="High">High</option>
-                </select>
-                <button class="addtodo">+</button>
-            </section>
-            <section id="todolist-${project.name}"></section>
+             
+            <div class="projects" class="max-w-sm w-full lg:max-w-full lg:flex">
+            <div>
+                <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+                </div>
+                <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div class="mb-8">
+                    <p class="text-sm text-gray-600 flex items-center">
+                        <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                        </svg>
+                        ${project.name}
+                    </p>
+                    <div class="text-gray-900 font-bold text-xl mb-2">${project.name}</div>
+                    </div>
+                    <div><h1 id="addproject" type="submit"><i class="fa fa-trash" aria-hidden="true" id="icon"></i></h1></div>
+                    <input type="text" id="title" name="title" placeholder="title" class="logo">
+                    <input type="date" id="date" name="date" placeholder="date">
+                    <select name="priority" id="priority">
+                        <option value="Low">Low</option>
+                        <option value="Medium">Medium</option>
+                        <option value="High">High</option>
+                    </select><div class="flex items-center">
+                    <div class="text-sm">
+                        <p class="text-gray-600">Aug 18</p>
+                    </div>
+                    </div>
+                <section id="todolist-${project.name}"></section>
+                </div>
+                </div>
+                </section>
+
             `;
         container.insertAdjacentHTML('afterbegin', proj);
     };
