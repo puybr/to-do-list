1889177e39603c3a3f01dc84873471f2c27ebf8a
Chloe Urisohn <56723522+ghost-goblin@users.noreply.github.com>
2024-12-15 13:21:32 +0200

    some updates

--- src/projects.js
+++ src/projects.js
@@ -7,11 +7,11 @@ const container = document.querySelector('#container');
 const projectManager = () => {
     const renderProject = (project) => {
         const proj = `       
-            <div class="projects max-w-sm w-full lg:max-w-full lg:flex">
+            <div class="projects">
             <div>
-                <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+                <div>
                 </div>
-                <div class="p-4 flex flex-col justify-between leading-normal">
+                <div>
                     <div class="mb-8">
                     <p class="flex items-center">
                         <i class="fa fa-trash fill-current w-3 h-3 mr-2" aria-hidden="true"></i>
