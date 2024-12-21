01dea0d64ccc2c3428d2087f3e9c1c345af12237
Chloe Urisohn <56723522+ghost-goblin@users.noreply.github.com>
2024-12-21 15:43:07 +0200

    some updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 "use strict";
 import './style.css';
 
+import navManager from './navbar';
 import controller from './controller';
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+
+// Navigation
+const nav = navManager();
+nav.render();
 
 // Set up the main controller
 const list = controller();
@@ -16,7 +21,7 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
     document.body.classList.add('dark');
   } else {
     document.body.classList.remove('dark');
-  }
+}
   
 // User actions to explicitly choose themes
 function toggleTheme(isDark) {
