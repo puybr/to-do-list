d13f5c322063f6d47fdd6730ddb89a15a74ff6bb
Chloe Urisohn <56723522+ghost-goblin@users.noreply.github.com>
2025-01-18 19:13:02 +0000

    some updates

--- src/projects.js
+++ src/projects.js
@@ -13,10 +13,10 @@ const projectManager = () => {
         });
         const form = `
             <form class="max-w-sm mx-auto">
-            <label for="projects" class="block mb-2 text-sm font-medium text-gray-900">select a project</label>
             <select id="projects" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5">
             ${options}
             </select>
+            <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">select</button>
             </form>
             `;
         container.insertAdjacentHTML('afterbegin', form);
