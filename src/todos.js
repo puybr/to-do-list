bc268274f64f56eb29d25beea638cf2568dba4d3
Chloe Urisohn <56723522+ghost-goblin@users.noreply.github.com>
2025-01-25 11:53:41 +0000

    some updates

--- src/todos.js
+++ src/todos.js
@@ -51,11 +51,18 @@ const todoManager = () => {
     const displayTodoForm = () => {
         const form = `
             <form class="w-full max-w-sm">
-            <div class="flex items-center border-green-500">
                 <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="new todo" aria-label="todo">
+                <input type="date" placeholder="select date">
+                <textarea></textarea>
+                <label>
+                <select name="projects" class="projects bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block p-2.5">
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
+                </select>
+                </label>
                 <button class="flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">+</button>
                 <button class="cancel flex-shrink-0 border-transparent border-4 text-green-500 hover:text-green-800 text-sm py-1 px-2 rounded" type="button">cancel</button>
-            </div>
             </form>`;
         todoForm .insertAdjacentHTML('afterbegin', form);
     };
