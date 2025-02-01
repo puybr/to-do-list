9213fdb33cda7a396eddb6367d500ba16c08cb3d
Chloe Urisohn <56723522+ghost-goblin@users.noreply.github.com>
2025-02-01 10:33:04 +0000

    some updates

--- src/projects.js
+++ src/projects.js
@@ -36,7 +36,7 @@ const projectManager = () => {
     };
     const displayProjectForm = () => {
         const form = `
-            <form class="w-full max-w-sm">
+            <form class="w-full max-w-sm p-4">
             <div class="flex items-center border-green-500">
                 <input id="newProject" class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="new project" aria-label="project">
                 <button id="submitProject" class="flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">+</button>
